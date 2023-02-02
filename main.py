44c6d6da5fee9611bf76ccf1d29016319948b0c3
marcelomatoss <marcelo.emboaba@gmail.com>
2023-02-02 15:57:46 -0300

    main update

--- main.py
+++ main.py
@@ -1,26 +1,3 @@
-from typing import Literal, Optional
-from uuid import uuid4
-from fastapi import FastAPI, HTTPException, Depends
-from pydantic import BaseModel
-import models
-from database import engine, SessionLocal
-from sqlalchemy.orm import Session
+from fastapi import FastAPI
 
 app = FastAPI()
-
-models.Base.metadata.create_all(bind=engine)
-
-def get_db()
-    try: 
-        db = SessionLocal()
-        yield db
-    finally:
-        db.close()
-
-class User(BaseModel):
-    name: str
-    lastname:str
-    user_id: Optional[str] = uuid4().hex
-    age: int
-    genre:Literal["Male", "Female"]
-    
