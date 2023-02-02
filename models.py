ccee9a03e52d51ae5f518a3dc0beb8556ebdd923
marcelomatoss <marcelo.emboaba@gmail.com>
2023-02-02 13:20:09 -0300

    Update models

--- models.py
+++ models.py
@@ -2,10 +2,10 @@ from sqlalchemy import Column, Integer, String
 from database import Base
 
 
-class Users(Base):
-    __tablename__ = "users"
+class User(Base):
+    __tablename__ = "user"
 
-    user_id = Column(Integer, primary_key = True, index = True)
+    user_id = Column(Integer, primary_key=True, index=True)
     name = Column(String)
     lastname =  Column(String)
     age = Column(Integer)
