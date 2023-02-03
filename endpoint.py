09936820407e25fd1729332091effd2f97d6a587
rubensm33 <rubensmarcelo31@gmaIl.com>
2023-02-03 12:50:55 -0300

    Adding funcionality list-user

--- endpoint.py
+++ endpoint.py
@@ -1,7 +1,16 @@
-from fastapi import FastAPI
-
+import models
+import schemas.users
+import database
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+import db
 app = FastAPI()
 
 @app.get("/")
 async def home():
     return {"message":"Folzeck Group"}
+
+@app.get("/list-user")
+async def list_user(user: schemas.users.UserBase, db: Session = Depends(db.get_db)):
+    return db.query(models.User).all()
+
