a1c006ac30210a66e7b0d5c340239d5909fffa1b
marcelomatoss <marcelo.emboaba@gmail.com>
2023-02-03 15:39:57 -0300

    ListUser by Index creation

--- endpoint.py
+++ endpoint.py
@@ -1,7 +1,8 @@
 from models import User
 from schemas.users import UserBase
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, HTTPException, Depends
 from sqlalchemy.orm import Session
+import models
 import db
 app = FastAPI()
 
@@ -13,3 +14,13 @@ async def home():
 async def list_user(user: UserBase, db: Session = Depends(db.get_db)):
     return db.query(User).all()
 
+@app.get("/list-user-by-index/{index}")
+async def list_user_by_index(index:int,db:Session=Depends(db.get_db)):
+    user_model=db.query(models.Users).filter(models.Users.user.id==index).first()
+    if user_model is None:
+        raise HTTPException(
+            status_code=404,
+            detail=f"ID{index}:Does not exist"
+        )
+    else:
+        return db.query(models.Users).filter(models.Users.user_id==index).first()
