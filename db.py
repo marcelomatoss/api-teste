e2cb4f2beae6bb701920ee2deb7d39c5f0bed04d
marcelomatoss <marcelo.emboaba@gmail.com>
2023-02-03 09:30:11 -0300

    EndPoint Home Creation

---
+++ db.py
@@ -0,0 +1,11 @@
+import models
+from database import engine, SessionLocal
+
+models.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    try: 
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
