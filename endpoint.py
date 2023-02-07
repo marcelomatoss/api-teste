521748d063e48746a041556ce1037cc34313cc2a
marcelomatoss <marcelo.emboaba@gmail.com>
2023-02-07 11:12:40 -0300

    Delete creation

--- endpoint.py
+++ endpoint.py
@@ -47,3 +47,23 @@ async def put_user(user:PutUser, index:int, db:Session=Depends(get_db)):
 
     return user
 
+    # /delete-user-by-index/{index} -> Delete User by Index
+@app.delete("/delete-user/{index}")
+async def delete_user_by_index(index:int,db:Session=Depends(get_db)):
+    user=db.query(User).filter(User.user_id==index).first()
+
+    if user is None:
+        raise HTTPException(
+            status_code=404,
+            detail=f"ID{index}:Does not exist"
+        )
+    db.query(User).filter(User.user_id==index).delete()
+    db.commit()
+
+#/delete-all-users -> Delete all users from database
+@app.delete("/delete-all-users")
+async def delete_all_users(db:Session=Depends(get_db)):
+
+    db.query(User).delete()
+    db.commit()
+
